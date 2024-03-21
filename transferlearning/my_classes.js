06e2f1324e7a0a6a70581997fccdc240bd40341a
Asta <201806556@post.au.dk>
2024-03-21 10:27:35 +0100

    Added my own model

--- transferlearning/my_classes.js
+++ transferlearning/my_classes.js
@@ -1,10 +1,5 @@
 export const IMAGENET_CLASSES = {
-  0: 'bird',
-  1: 'cat',
-  2: 'cows',
-  3: 'dog',
-  4: 'horse',
-  5: 'mouse',
-  6: 'pig',
-  7: 'sheep'
+  0: 'baklava',
+  1: 'hamburger',
+  2: 'chicken wings'
 };
